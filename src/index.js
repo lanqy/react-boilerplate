6c218caabcb761e23f24c3add5fa1e66ad261530
Lan Qingyong <lanqy1314@gmail.com>
2016-07-19 17:19:08 +0800

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import About from './containers/About';
+import Projects from './containers/Projects';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="about" component={About} />
+        <Route path="projects" component={Projects} />
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('container')
+);
