329f91106351002721179b6a7ee8f7ae5fc62f4e
Lan Qingyong <lanqy1314@gmail.com>
2016-07-20 17:29:08 +0800

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,37 @@
-export const PROJECT_ACTION = 'PROJECT_ACTION';
-export const ANOTHER_PROJECT_ACTION = 'ANOTHER_PROJECT_ACTION';
-export const ABOUT_ACTION = 'ABOUT_ACTION';
-export const ANOTHER_ABOUT_ACTION = 'ANOTHER_ABOUT_ACTION';
+let nextProjectId = 0;
+let nextAboutId = 0;
+
+export const ADD_PROJECTS = 'ADD_PROJECTS';
+export const DEL_PROJECTS = 'DEL_PROJECTS';
+export const ADD_ABOUT = 'ADD_ABOUT';
+export const DEL_ABOUT = 'DEL_ABOUT';
+
+export const addProject = (text) => {
+  return {
+    type:ADD_PROJECTS,
+    id:nextProjectId++,
+    text
+  }
+}
+
+export const delProject = (index) => {
+  return {
+    type:DEL_PROJECTS,
+    index
+  }
+}
+
+export const addAbout = (text) => {
+    return {
+      type:ADD_ABOUT,
+      id:nextAboutId++,
+      text
+    }
+}
+
+export const delAbout = (index) => {
+    return {
+      type:DEL_ABOUT,
+      index
+    }
+}
