c9c18efef9e196a9f1b58bd4de774077ed8d4c52
Lan Qingyong <lanqy1314@gmail.com>
2016-07-23 01:44:36 +0800

    Add files via upload

--- src/containers/list.js
+++ src/containers/list.js
@@ -5,18 +5,9 @@ import * as Actions from '../actions';
 
 class List extends React.Component {
 
-  constructor(props, context) { // 获取上下文
-      super(props, context);
-      this.state = {projects: this.context.store.getState().project};
-      console.log("context", this.context);
-      console.log("props1:", this.props);
-      this.props.addProject("4444444");
-  }
-
   render(){
-    this.props.addProject("6666");
-    const store = this.context.store;
-    const items = this.state.projects.map((item,i) => {
+   console.log("props log",this.props);
+    const items = this.props.state.map((item,i) => {
       return <Item item={item} key={i} onClick={() => this.remove(i)} />
     });
 
@@ -25,25 +16,17 @@ class List extends React.Component {
     )
   }
 
-  remove(i,e){
-    console.log(i);
-    console.log(this.props);
+  remove(i){
+    console.log(i,this.props);
     this.props.delProject(i);
-    
-    //console.log(store);
-  //  store.dispatch(Actions.delProject(index));
-  //  this.updateState(store)
-  }
-
-  updateState(store){ // update
-    this.setState({
-      projects:store.getState().project
-    });
   }
 }
 
 function mapStateToProps(state) {
-    return {};
+   console.log("state:",state.project);
+    return {
+      state:state.project
+    };
 }
 
 function mapDispatchToProps(dispatch) {
@@ -56,9 +39,5 @@ function mapDispatchToProps(dispatch) {
     }
   };
 }
-
-List.contextTypes = { // context
-  store: React.PropTypes.object.isRequired
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(List);
