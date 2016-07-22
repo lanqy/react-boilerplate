aafcdc3998c846953476ac42620e96e169e0beae
Lan Qingyong <lanqy1314@gmail.com>
2016-07-22 23:20:05 +0800

    Add files via upload

--- src/containers/list.js
+++ src/containers/list.js
@@ -1,20 +1,23 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
 import Item from './item';
 import * as Actions from '../actions';
 
-export default class List extends React.Component {
+class List extends React.Component {
 
   constructor(props, context) { // 获取上下文
       super(props, context);
       this.state = {projects: this.context.store.getState().project};
       console.log("context", this.context);
-      console.log("props:", this.props);
+      console.log("props1:", this.props);
+      this.props.addProject("4444444");
   }
 
   render(){
+    this.props.addProject("6666");
     const store = this.context.store;
     const items = this.state.projects.map((item,i) => {
-      return <Item item={item} key={i} onClick={this.remove} />
+      return <Item item={item} key={i} onClick={() => this.remove(i)} />
     });
 
     return (
@@ -22,8 +25,11 @@ export default class List extends React.Component {
     )
   }
 
-  remove(component,e){
-    console.log(component);
+  remove(i,e){
+    console.log(i);
+    console.log(this.props);
+    this.props.delProject(i);
+    
     //console.log(store);
   //  store.dispatch(Actions.delProject(index));
   //  this.updateState(store)
@@ -36,6 +42,23 @@ export default class List extends React.Component {
   }
 }
 
+function mapStateToProps(state) {
+    return {};
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addProject: function (text) {
+      return dispatch(Actions.addProject(text));
+    },
+    delProject: function(index){
+      return dispatch(Actions.delProject(index))
+    }
+  };
+}
+
 List.contextTypes = { // context
   store: React.PropTypes.object.isRequired
 };
+
+export default connect(mapStateToProps, mapDispatchToProps)(List);
